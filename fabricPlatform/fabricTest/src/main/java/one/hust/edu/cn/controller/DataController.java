8d4444a0b31dc2689eae164070be50db84b44049
1768001587 <1768001587@qq.com>
2020-12-25 17:43:47 +0800

    'jwt'

--- fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/DataController.java
+++ fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/DataController.java
@@ -1,13 +1,16 @@
 package one.hust.edu.cn.controller;
 
+import com.auth0.jwt.JWT;
 import lombok.extern.slf4j.Slf4j;
 import one.hust.edu.cn.entities.CommonResult;
 import one.hust.edu.cn.entities.MyFile;
+import one.hust.edu.cn.myAnnotation.CheckToken;
 import one.hust.edu.cn.service.DataService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -21,8 +24,9 @@ public class DataController{
     private DataService fileService;
 
     //上传文件
+    @CheckToken
     @PostMapping(value = "/uploadFile")
-    public CommonResult uploadFile(@RequestParam("file") MultipartFile file,@RequestParam("originUserId")Integer originUserId){
+    public CommonResult uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest httpServletRequest){
         //获取文件名
         String fileName = file.getOriginalFilename();
         String filePath = "D:/研究生资料/南六218实验室/代炜琦项目组文件/github同步代码/uploadFilePackage/";
@@ -38,6 +42,9 @@ public class DataController{
             size = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
             // 此时size就是保留两位小数的浮点数
             myFile.setDataSize(size);
+            // 从 http 请求头中取出 token
+            String token = httpServletRequest.getHeader("token");
+            Integer originUserId = JWT.decode(token).getClaim("id").asInt();
             myFile.setOriginUserId(originUserId);
             myFile.setDataType(fileName.substring(fileName.lastIndexOf("."))+"文件");
             //初次创建时将初始时间和修改时间写成一样
@@ -46,8 +53,7 @@ public class DataController{
             fileService.uploadFile(myFile);
             return new CommonResult<>(200,"上传成功，文件位于："+filePath+fileName,myFile);
         } catch (Exception e) {
-            e.printStackTrace();
-            return new CommonResult<>(400,"上传失败",null);
+            return new CommonResult<>(400,e.getMessage(),null);
         }
     }
     //获取文件列表
--- fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/UserController.java
+++ fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/UserController.java
@@ -1,9 +1,12 @@
 package one.hust.edu.cn.controller;
 
+import cn.hutool.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import one.hust.edu.cn.entities.CommonResult;
 import one.hust.edu.cn.entities.User;
+import one.hust.edu.cn.myAnnotation.LoginToken;
 import one.hust.edu.cn.service.UserService;
+import one.hust.edu.cn.utils.JwtUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,20 +20,34 @@ public class UserController{
     private UserService userService;
     //登录
     @PostMapping(value = "login")
+    @LoginToken
     public CommonResult login(@RequestBody User user){
+        JSONObject jsonObject = new JSONObject();
         boolean result = userService.login(user);
         if(result) {
             User uresult = userService.findUserByUsername(user.getUsername());
-            return new CommonResult<>(200,"登录成功",uresult);
+            String token = JwtUtil.createJWT(6000000, uresult);
+            jsonObject.put("user", uresult);
+            jsonObject.put("token", token);
+            return new CommonResult<>(200,"登录成功",jsonObject);
         }
-        else return new CommonResult<>(400,"登录失败,用户不存在或用户名或密码错误",null);
+        else {
+            return new CommonResult<>(400,"登录失败,用户不存在或用户名或密码错误",null);
+        }
     }
     //注册
     @PostMapping(value = "register")
+    @LoginToken
     public CommonResult register(@RequestBody User user){
+            JSONObject jsonObject = new JSONObject();
             if(userService.findUserByUsername(user.getUsername())!=null) return new CommonResult<>(400,"注册失败,用户名已存在",null);
             boolean result = userService.register(user);
-            if(result) return new CommonResult<>(200,"注册成功",user);
+            if(result) {
+                String token = JwtUtil.createJWT(6000000, user);
+                jsonObject.put("token", token);
+                jsonObject.put("user", user);
+                return new CommonResult<>(200,"注册成功",jsonObject);
+            }
             else return new CommonResult<>(400,"注册失败,请联系系统管理员",null);
     }
 }
