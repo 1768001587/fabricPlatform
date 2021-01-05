206c46f6224c37132339e3d4ecdc1bb8148c5d23
ZhaoWenHao1 <happyzhao1010@gmail.com>
2021-01-05 17:25:26 +0800

    增加fabric服务

--- fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/FabricTestMain8080.java
+++ fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/FabricTestMain8080.java
@@ -2,8 +2,10 @@ package one.hust.edu.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("one.hust.edu.cn.feign")
 public class FabricTestMain8080 {
     public static void main(String[] args) {
         SpringApplication.run(FabricTestMain8080.class,args);
--- fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/DataController.java
+++ fabricPlatform/fabricTest/src/main/java/one/hust/edu/cn/controller/DataController.java
@@ -9,6 +9,7 @@ import one.hust.edu.cn.myAnnotation.CheckToken;
 import one.hust.edu.cn.service.ChannelAuthorityService;
 import one.hust.edu.cn.service.DataAuthorityService;
 import one.hust.edu.cn.service.DataService;
+import one.hust.edu.cn.service.FabricService;
 import one.hust.edu.cn.utils.TxtUtil;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,19 +27,21 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-public class DataController{
+public class DataController {
     @Resource
     private DataService fileService;
     @Resource
     ChannelAuthorityService channelAuthorityService;
     @Resource
     DataAuthorityService dataAuthorityService;
+    @Resource
+    FabricService fabricService;
 
     //上传文件
     @CheckToken
     @PostMapping(value = "/data/uploadFile")
     @ResponseBody
-    public CommonResult uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("channelId") Integer channelId, HttpServletRequest httpServletRequest){
+    public CommonResult uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("channelId") Integer channelId, HttpServletRequest httpServletRequest) {
         //获取文件名
         String fileName = file.getOriginalFilename();
         String filePath = "D:/研究生资料/南六218实验室/代炜琦项目组文件/github同步代码/uploadFilePackage/";
@@ -46,15 +49,15 @@ public class DataController{
         String token = httpServletRequest.getHeader("token");
         Integer originUserId = JWT.decode(token).getClaim("id").asInt();
         List<String> list = channelAuthorityService.getAddAuthorityChannels(originUserId);
-        if(list.size()<1){
-            return new CommonResult<>(400,"您没有权限上传文件至任一channel",null);
+        if (list.size() < 1) {
+            return new CommonResult<>(400, "您没有权限上传文件至任一channel", null);
         }
         try {
             //进行文件传输
-            file.transferTo(new File(filePath+fileName));
+            file.transferTo(new File(filePath + fileName));
             MyFile myFile = new MyFile();
             myFile.setChannelId(channelId);//这里后面要做出选择channel
-            myFile.setDataName(filePath+fileName);
+            myFile.setDataName(filePath + fileName);
             //文件大小以KB作为单位
             // 首先先将.getSize()获取的Long转为String 单位为B
             Double size = Double.parseDouble(String.valueOf(file.getSize()));
@@ -65,28 +68,30 @@ public class DataController{
             myFile.setDataSize(size);
             myFile.setData(TxtUtil.getTxtContent(myFile));
             myFile.setOriginUserId(originUserId);
-            myFile.setDataType(fileName.substring(fileName.lastIndexOf("."))+"文件");
+            myFile.setDataType(fileName.substring(fileName.lastIndexOf(".")) + "文件");
             //初次创建时将初始时间和修改时间写成一样
             myFile.setCreatedTime(new Timestamp(new Date().getTime()));
             myFile.setModifiedTime(new Timestamp(new Date().getTime()));
             fileService.uploadFile(myFile);
-            return new CommonResult<>(200,"上传成功，文件位于："+filePath+fileName,myFile);
+            return new CommonResult<>(200, "上传成功，文件位于：" + filePath + fileName, myFile);
         } catch (Exception e) {
-            return new CommonResult<>(400,e.getMessage(),null);
+            return new CommonResult<>(400, e.getMessage(), null);
         }
     }
+
     //获取文件列表
     @GetMapping(value = "/data/getDataList")
-    public CommonResult getDataList(){
+    public CommonResult getDataList() {
         List<MyFile> list = fileService.getDataList();
-        return new CommonResult<>(200,"获取所有文件列表成功",list);
+        return new CommonResult<>(200, "获取所有文件列表成功", list);
     }
+
     //根据文件id删除文件
     //上传文件
     @CheckToken
     @PostMapping(value = "/data/deleteDataById")
     @ResponseBody
-    public CommonResult deleteDataById(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest){
+    public CommonResult deleteDataById(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest) {
         Integer dataId = Integer.valueOf(params.get("dataId"));
         // 从 http 请求头中取出 token
         String token = httpServletRequest.getHeader("token");
@@ -97,25 +102,26 @@ public class DataController{
         dataAuthority.setDataSampleId(dataId);
         dataAuthority.setAuthorityKey(3);//3代表有删除的权限
         System.out.println(dataAuthorityService.checkDataAuthority(dataAuthority));
-        if(dataAuthorityService.checkDataAuthority(dataAuthority)<1){
-            return new CommonResult<>(400,"操作失败，您不存在对此文件删除的权限",null);
+        if (dataAuthorityService.checkDataAuthority(dataAuthority) < 1) {
+            return new CommonResult<>(400, "操作失败，您不存在对此文件删除的权限", null);
         }
         Integer result = fileService.deleteDataById(dataId);
-        if(result<1){
-            return new CommonResult<>(400,"不存在id为："+dataId+"的文件",null);
+        if (result < 1) {
+            return new CommonResult<>(400, "不存在id为：" + dataId + "的文件", null);
         }
-        return new CommonResult<>(200,"成功删除id为："+dataId+"的文件,token为："+token,null);
+        return new CommonResult<>(200, "成功删除id为：" + dataId + "的文件,token为：" + token, null);
     }
+
     //根据文件id获取文件内容
     @CheckToken
     @PostMapping(value = "/data/getData")
     @ResponseBody
-    public CommonResult getData(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest){
+    public CommonResult getData(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest) {
         Integer dataId = Integer.valueOf(params.get("dataId"));
         MyFile result = fileService.findDataById(dataId);
 
-        if(result==null){
-            return new CommonResult<>(400,"不存在id为："+dataId+"的文件",null);
+        if (result == null) {
+            return new CommonResult<>(400, "不存在id为：" + dataId + "的文件", null);
         }
         // 从 http 请求头中取出 token
         String token = httpServletRequest.getHeader("token");
@@ -126,22 +132,23 @@ public class DataController{
         dataAuthority.setDataSampleId(dataId);
         dataAuthority.setAuthorityKey(1);//1代表有查看的权限
         System.out.println(dataAuthorityService.checkDataAuthority(dataAuthority));
-        if(dataAuthorityService.checkDataAuthority(dataAuthority)<1){
-            return new CommonResult<>(400,"操作失败，您不存在对此文件查看的权限",null);
+        if (dataAuthorityService.checkDataAuthority(dataAuthority) < 1) {
+            return new CommonResult<>(400, "操作失败，您不存在对此文件查看的权限", null);
         }
         String txtContent = TxtUtil.getTxtContent(result);
-        return new CommonResult<>(200,"文件token为："+token,txtContent);
+        return new CommonResult<>(200, "文件token为：" + token, txtContent);
     }
+
     //根据文件id对文件内容进行更新
     @CheckToken
     @PostMapping(value = "/data/updateData")
     @ResponseBody
-    public CommonResult updateData(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest){
+    public CommonResult updateData(@RequestBody Map<String, String> params, HttpServletRequest httpServletRequest) {
         Integer dataId = Integer.valueOf(params.get("dataId"));
         String dataContent = params.get("dataContent");
         MyFile myFile = fileService.findDataById(dataId);
-        if(myFile==null){
-            return new CommonResult<>(400,"不存在id为："+dataId+"的文件",null);
+        if (myFile == null) {
+            return new CommonResult<>(400, "不存在id为：" + dataId + "的文件", null);
         }
         // 从 http 请求头中取出 token
         String token = httpServletRequest.getHeader("token");
@@ -152,8 +159,8 @@ public class DataController{
         dataAuthority.setDataSampleId(dataId);
         dataAuthority.setAuthorityKey(2);//2代表有修改的权限
         System.out.println(dataAuthorityService.checkDataAuthority(dataAuthority));
-        if(dataAuthorityService.checkDataAuthority(dataAuthority)<1){
-            return new CommonResult<>(400,"操作失败，您不存在对此文件修改的权限",null);
+        if (dataAuthorityService.checkDataAuthority(dataAuthority) < 1) {
+            return new CommonResult<>(400, "操作失败，您不存在对此文件修改的权限", null);
         }
         File old_file = new File(myFile.getDataName());
         old_file.delete();
@@ -177,6 +184,6 @@ public class DataController{
         myFile.setDataSize(size);
         myFile.setModifiedTime(new Timestamp(new Date().getTime()));
         fileService.updateFile(myFile);
-        return new CommonResult<>(200,"id为："+dataId+"的文件更新成功",null);
+        return new CommonResult<>(200, "id为：" + dataId + "的文件更新成功", null);
     }
 }
