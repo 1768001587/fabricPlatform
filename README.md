528af03163919dd6cc24a6586279221fc4f864ce
ZhaoWenHao1 <happyzhao1010@gmail.com>
2021-01-10 19:32:28 +0800

    增加swagger

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fabricPlatform
 基于fabric的测试平台
+## Swagger
+### 注解说明
+- @Api：用于修饰Controller类，生成Controller相关文档信息
+- @ApiOperation：用于修饰Controller类中的方法，生成接口方法相关文档信息
+- @ApiParam：用于修饰接口中的参数，生成接口参数相关文档信息
+- @ApiModelProperty：用于修饰实体类的属性，当实体类是请求参数或返回结果时，直接生成相关文档信息
--- fabricPlatform/system/src/main/java/com/hust/keyRD/system/KeyRDMain.java
+++ fabricPlatform/system/src/main/java/com/hust/keyRD/system/KeyRDMain.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients("com.hust.keyRD.system.api.feign")
